a0be44bef91827871fada4885a553cc38984edb0
luorigong <luorigong@wacai.com>
2019-03-28 12:02:41 +0800

    fix: @Repository->@Service

--- src/main/java/cn/wudi/spider/robot/topsummary/FindTopicSummary.java
+++ src/main/java/cn/wudi/spider/robot/topsummary/FindTopicSummary.java
@@ -3,10 +3,12 @@ package cn.wudi.spider.robot.topsummary;
 import static cn.wudi.spider.constant.Constant.TOP_PREFIX;
 import static cn.wudi.spider.constant.Constant.TOP_SUFFIX;
 
+import cn.wudi.spider.robot.base.Find;
+
 /**
  * @author wudi
  */
-public class FindTopicSummary {
+public class FindTopicSummary extends Find {
 
 
   public String findTopAnswersNum(String topicId) {
--- src/main/java/cn/wudi/spider/service/SpiderServiceImpl.java
+++ src/main/java/cn/wudi/spider/service/SpiderServiceImpl.java
@@ -6,16 +6,17 @@ import cn.wudi.spider.entity.Result;
 import cn.wudi.spider.entity.SummaryQuery;
 import cn.wudi.spider.entity.SummaryResult;
 import cn.wudi.spider.logger.support.LocalLoggerFactory;
+import cn.wudi.spider.robot.base.AbstractFind;
 import cn.wudi.spider.robot.base.DefaultHttpClientFactory;
 import cn.wudi.spider.robot.base.HttpClientFactory;
 import cn.wudi.spider.robot.topid.FindTopicId;
 import cn.wudi.spider.robot.topsummary.FindTopicSummary;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 /**
  * @author wudi
  */
-@Repository
+@Service
 public class SpiderServiceImpl implements SpiderService {
 
   private final HttpClientFactory httpClientFactory;
@@ -31,10 +32,11 @@ public class SpiderServiceImpl implements SpiderService {
   public <T extends CommonQuery> Result create(T query) {
     CreateResult createResult = new CreateResult();
     String topicTitle = query.getTopicTitle();
-    createResult.setTopicTitle(topicTitle);
     FindTopicId findTopicId = new FindTopicId();
-    findTopicId.setLogger(localLoggerFactory.newLogger(topicTitle));
-    findTopicId.setClient(httpClientFactory.getClient(topicTitle));
+
+    createLoggerAndClient(findTopicId, topicTitle);
+
+    createResult.setTopicTitle(topicTitle);
     createResult.setTopicId(findTopicId.findTopId(topicTitle));
     return Result.ok(createResult);
   }
@@ -44,10 +46,18 @@ public class SpiderServiceImpl implements SpiderService {
     SummaryResult summaryResult = new SummaryResult();
     String topicId = ((SummaryQuery) query).getTopicId();
     FindTopicSummary findTopicSummary = new FindTopicSummary();
+
+    createLoggerAndClient(findTopicSummary, topicId);
+
     summaryResult.setTopicTitle(query.getTopicTitle());
     summaryResult.setTopicId(topicId);
     summaryResult.setTopicAnswerNum(findTopicSummary.findTopAnswersNum(topicId));
     summaryResult.setTopicUrl(findTopicSummary.findTopUrl(topicId));
     return Result.ok(summaryResult);
+  }
+
+  private void createLoggerAndClient(AbstractFind fund, String id) {
+    fund.setClient(httpClientFactory.getClient(id));
+    fund.setLogger(localLoggerFactory.newLogger(id));
   }
 }
